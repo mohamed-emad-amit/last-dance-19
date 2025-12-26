fdb23f1303432c41c7b3edcacd6c751e87741e4d
mohamed-emad-amit <mohamed-emad@amit-learning.com>
2025-12-26 18:54:21 +0200

    implement global navbar - add new package [react-icons]

--- src/components/GlobalNavbar/GlobalNavbar.jsx
+++ src/components/GlobalNavbar/GlobalNavbar.jsx
@@ -1,5 +1,38 @@
-import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { SiWoocommerce } from "react-icons/si";
+import { Link } from "react-router-dom";
 
 export const GlobalNavbar = () => {
-  return <div>GlobalNavbar</div>;
+  return (
+    <Navbar expand="md" bg="light" data-bs-theme="light">
+      <Container>
+        {/* Brand With Logo Only */}
+        <Navbar.Brand as={Link} to="/">
+          <SiWoocommerce className="display-3 text-primary" />
+        </Navbar.Brand>
+
+        {/* Toggler Button */}
+        <Navbar.Toggle />
+
+        {/* Collapse Nav */}
+        <Navbar.Collapse>
+          <Nav>
+            {/* Home Link */}
+            <Nav.Item>
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+            </Nav.Item>
+
+            {/* Login Link */}
+            <Nav.Item>
+              <Nav.Link as={Link} to="/login">
+                Login
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
